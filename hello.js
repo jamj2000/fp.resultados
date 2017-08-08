de2c0765984933d1848ffb2a9ea3551cf7d40059
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-08-08 21:02:03 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\nSoon something great comes to this site!');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(443);
+console.log('Server running at https://127.0.0.1/');
