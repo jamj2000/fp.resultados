d1d8586af14a5453306e780d66c949b4a5a56e39
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-08-11 17:57:37 +0200

    update

--- hello.js
+++ hello.js
@@ -1,9 +1,14 @@
-var http = require('http');
+const http = require('http');
 var port=Number(process.env.PORT || 5000);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!\n');  
-}).listen(process.env.PORT || 5000);
+//  res.end('¡Hola mundo!\n');  
 
-console.log('Server running on ', port);
+  const ip = req.socket.remoteAddress;
+  const port = req.socket.remotePort;
+  res.end(`Tu IP es ${ip} y tu puerto es ${port}.`);
+
+}).listen(port);
+
+console.log('Servidor ejecutandose en el puerto ', port);
