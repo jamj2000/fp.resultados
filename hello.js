090801e11e7aac9355dcbaa2c37c525b4611f1dc
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-08-11 17:40:23 +0200

    update

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var port=Number(process.env.PORT || 3000);
+var port=Number(process.env.PORT || 5000);
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!\n');  
-}).listen(port);
+}).listen(process.env.PORT || 5000);
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running on ', port);
