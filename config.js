26ea9d49142cca9ee403c46f0e7321c37d03bd9b
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-08-26 00:19:38 +0200

    Modificadas variables APP_SERVICE_*

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // El primer valor es el de PRODUCCIÓN. El valor alternativo es el de DESARROLLO
 
 module.exports = {
-  ip         : process.env.FP_RESULTADOS2_SERVICE_HOST        || '127.0.0.1',
-  port       : process.env.FP_RESULTADOS2_SERVICE_PORT_WEB    || 3000,
-  db_host    : process.env.MONGODB_SERVICE_HOST               || '127.0.0.1',
-  db_port    : process.env.MONGODB_SERVICE_PORT               || 27017,
-  db_user    : process.env.MONGODB_USER                       || '',
-  db_password: process.env.MONGODB_PASSWORD                   || '',
-  db_name    : process.env.MONGODB_DATABASE                   || 'fp'
+  ip         : process.env.APP_SERVICE_HOST        || '127.0.0.1',
+  port       : process.env.APP_SERVICE_PORT_WEB    || 3000,
+  db_host    : process.env.MONGODB_SERVICE_HOST    || '127.0.0.1',
+  db_port    : process.env.MONGODB_SERVICE_PORT    || 27017,
+  db_user    : process.env.MONGODB_USER            || '',
+  db_password: process.env.MONGODB_PASSWORD        || '',
+  db_name    : process.env.MONGODB_DATABASE        || 'fp'
 };
