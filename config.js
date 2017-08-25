dc3cc1d30406f89fff453a26a13ceae2b967d70e
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2017-08-26 00:06:49 +0200

    Arreglado tipo de comas

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   ip         : process.env.FP_RESULTADOS2_SERVICE_HOST        || '127.0.0.1',
   port       : process.env.FP_RESULTADOS2_SERVICE_PORT_WEB    || 3000,
   db_host    : process.env.MONGODB_SERVICE_HOST               || '127.0.0.1',
-  db_port    : process.env.MONGODB_SERVICE_PORT               || 27017
-  db_user    : process.env.MONGODB_USER                       || ''
-  db_password: process.env.MONGODB_PASSWORD                   || ''
+  db_port    : process.env.MONGODB_SERVICE_PORT               || 27017,
+  db_user    : process.env.MONGODB_USER                       || '',
+  db_password: process.env.MONGODB_PASSWORD                   || '',
   db_name    : process.env.MONGODB_DATABASE                   || 'fp'
 };
